6705fde4176187c7f1b163a7e51744a023aca1c2
Bharat Kathi <bharat1031@gmail.com>
2025-07-24 01:24:27 -0700

    web: init:

---
+++ web/src/consts/config.tsx
@@ -0,0 +1,14 @@
+export const BACKEND_URL =
+  import.meta.env.VITE_BACKEND_URL ?? "https://vpn-api.gauchoracing.com";
+
+export const SENTINEL_OAUTH_BASE_URL =
+  "https://sso.gauchoracing.com/oauth/authorize";
+export const SENTINEL_CLIENT_ID = "aLrhpakeWRop";
+
+export const SOCIAL_LINKS = {
+  github: "https://github.com/gaucho-racing/grvpn",
+  instagram: "https://instagram.com/gauchoracingucsb",
+  twitter: "https://twitter.com/gauchoracing_",
+  linkedin:
+    "https://www.linkedin.com/company/gaucho-racing-at-uc-santa-barbara",
+};
